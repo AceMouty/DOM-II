f6092365d0fb600147152cf1baf1104ebb8fed3c
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-08-06 18:11:14 -0500

    Completed MVP

--- js/index.js
+++ js/index.js
@@ -2,8 +2,10 @@
 
 const header = document.querySelector('header');
 const h1 = document.querySelector('h1');
+const h2 = document.querySelectorAll('h2');
 const nav = document.querySelector('nav');
 const div = document.querySelectorAll('div');
+const btn = document.querySelector('div.btn');
 const section = document.querySelectorAll('section');
 const imgs = document.querySelectorAll('img');
 const buffer = [];
@@ -26,17 +28,15 @@ window.addEventListener('load', (e) => {
 		e.target.style.backgroundColor = 'lime';
 	})
 
+	header.addEventListener('mouseleave', (e) =>{
+		e.target.style.backgroundColor = 'purple';
+	})
+
 	// Key Down Event
 	document.body.addEventListener('keydown', (e) => {
 		buffer.push(`Character: ${e.code}, Character Code: ${e.keyCode}`);
 	})
 
-	// Wheel Listeners
-	div.forEach(el => {
-		el.addEventListener('wheel', (e) => {
-			e.target.style.backgroundColor = `${colors[Math.floor(Math.random()*colors.length)]}`
-		})
-	})
 
 	section.forEach(el => {
 		el.addEventListener('wheel', (e) => {
@@ -49,7 +49,7 @@ window.addEventListener('load', (e) => {
 		alert(`You just double clicked on a h1 tag, very nice it goes no where, we will fix this soon`);
 	})
 
-	// Double Click Event
+	
 	nav.addEventListener('dblclick', (e) => {
 		if(e.target.tagName === "A"){
 			alert(`You just double clicked on a link! But it goes nowhere sorry.`)
@@ -58,13 +58,18 @@ window.addEventListener('load', (e) => {
 		}
 	})
 
+	nav.addEventListener('click', (e) => {
+		e.preventDefault();
+	})
+
 	
 	imgs.forEach(img => {
+		img.addEventListener('dblclick', (e) => {
+			alert('You just double clicked an image on the page...NICE!')
+			e.stopPropagation();
+	})
 
-		// Double click
-		img.addEventListener('dblclick', () => alert('You just double clicked an image on the page...NICE!'))
-
-		// mouseenter 
+		
 		img.addEventListener('mouseenter', (e) => {
 			e.target.parentElement.style.width = "300%";
 			e.target.style.width = '100%';
@@ -76,5 +81,49 @@ window.addEventListener('load', (e) => {
 		})
 	})
 
+	h2.forEach(header => {
+		header.addEventListener('dblclick', (e) => {
+			alert('You double clicked an h2 element!');
+			// e.stopPropagation();
+		})
+
+		header.addEventListener('mouseover', (e) => {
+			e.target.style.color = `${colors[Math.floor(Math.random()*colors.length)]}`;
+			setTimeout(() => e.target.style.color = 'black', 500);
+		});
+	})
+
+	// Dragstart & Dragend
+	imgs[0].addEventListener('dragstart', dragstart);
+	imgs[0].addEventListener('dragend', dragend);
+	
+
+	function dragstart (){
+			 setTimeout(() => this.style.display = 'none',0);
+			// console.log('dragstart');
+	};
+
+	function dragend (){
+		this.style.display = 'inline';
+		// console.log('end');
+	};
+
+
+	// Wheel Listeners
+	div.forEach(el => {
+		el.addEventListener('wheel', (e) => {
+			e.target.style.backgroundColor = `${colors[Math.floor(Math.random()*colors.length)]}`
+		})
+
+	})
+
+	div[6].addEventListener('click', (e) => {
+		e.target.style.fontSize = '8px';
+	})
+
+	btn.addEventListener('click', (e) => {
+		e.target.style.color = `${colors[Math.floor(Math.random()*colors.length)]}`;
+		e.stopPropagation();
+	})
 
 })
